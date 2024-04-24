b756d10d86258c8a79849e51952d8fea5d162278
BuildWithCloud <itried100bears@outlook.com>
2024-04-24 20:07:39 +0100

    Added rotation to the hub

--- Functions/Setup.py
+++ Functions/Setup.py
@@ -1,13 +1,14 @@
 from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop, Axis
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
 class Gary:
     #Hub
-    hub = PrimeHub()
+    hub = PrimeHub(top_side=Axis.Z, front_side=-Axis.Y)
     hub.display.orientation(Side.LEFT)
+    hub.system.set_stop_button(Button.BLUETOOTH)
 
     #Sensors
     LCol = ColorSensor(Port.A)
