2393ce3e1e26f00bf5430e2feec0ec588a97ea0c
BuildWithCloud <itried100bears@outlook.com>
2024-04-25 08:25:00 +0100

    Fixed program ending before run finished

--- Code/main.py
+++ Code/main.py
@@ -2,8 +2,11 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait, StopWatch, run_task
 import Setup
 import DriveBaseTest
 
-DriveBaseTest.DriveBaseTest()
+def main():
+    DriveBaseTest.DriveBaseTest()
+
+run_task(main)
