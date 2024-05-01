39ba04007ddab90f0892aa606d1f7ac8577b09cf
BuildWithCloud <127132530+BuildWithCloud@users.noreply.github.com>
2024-05-01 13:42:02 +0100

    Update LineFunctions.py

--- Code/LineFunctions.py
+++ Code/LineFunctions.py
@@ -3,8 +3,23 @@ from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSenso
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
-
+#import math
 import Setup
-def LineSquare():
-    Setup.Bary.Drive.straight
+#def LineSquare():
 
+def AngleToLine():
+    Setup.Bary.Drive.drive(100,0)
+    while Setup.Bary.LCol.reflection() > 25 and Setup.Bary.RCol.reflection() > 25:
+        pass
+    dIstance = Setup.Bary.Drive.distance()
+    if Setup.Bary.LCol.reflection() < 25:
+        Setup.Bary.hub.display.pixel(1, 1, 100)
+        while Setup.Bary.RCol.reflection() > 25:
+            pass
+        move = Setup.Bary.Drive.distance() - distance
+    else:
+        pass
+    Setup.Bary.Drive.stop()
+    wait(1000)
+    Setup.Bary.Drive.straight(distance=dIstance)
+    return (math.atan(dIstance/Setup.Bary.ColDistance))
